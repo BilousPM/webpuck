cdc0074d13ec541c5c7ff2b582290bdd381ab403
BilousPM <bilouspm@gmail.com>
2024-05-02 11:03:00 -0600

    create start files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import name from './module-1';
+import './css/common.css';
+import './css/main.scss';
+
+console.log(name);
+
+console.log('it is index.js.');
+console.log('bfrsnfnfr');
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'my-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 5555,
+  },
+};
